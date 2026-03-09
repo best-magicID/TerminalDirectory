6d86ef32e48cd68719704eb994ede32f101d4c4c
Андрей Полчихин <Polchihin@yandex.ru>
2026-03-09 21:35:41 +0500

    Добавил DellinDictionaryDbContext

---
+++ task/Infrastructure/DellinDictionaryDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using task.Entities;
+
+namespace task.Infrastructure
+{
+    public class DellinDictionaryDbContext : DbContext
+    {
+        public DbSet<Office> Offices { get; set; }
+
+
+        public DellinDictionaryDbContext(
+            DbContextOptions<DellinDictionaryDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(
+                Assembly.GetExecutingAssembly());
+        }
+    }
+}
